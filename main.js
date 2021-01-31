39f05b953ba9a6bbc0ea91bb93c55ed8b84840e6
Otavio Sonda Alenski <otavio.alenski@tivix.com>
2021-01-31 13:15:11 +0000

    Updates

--- main.js
+++
@@ -1,9 +0,0 @@
-const header = document.getElementsByTagName("header")[0];
-
-header.children[0].addEventListener('mouseover', () => {
-    header.children[1].classList.add('over')
-})
-
-header.children[0].addEventListener('mouseleave', () => {
-    header.children[1].classList.remove('over')
-})
